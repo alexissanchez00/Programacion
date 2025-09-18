b530e12c6be44c3d56104dab1942285b4fbcac5f
alexissanchez00 <cesaralexsanchezcorrea48@gmail.com>
2025-09-18 07:16:04 -0600

    si

---
+++ Unidad2/comentario.py
@@ -0,0 +1,10 @@
+#Este prgrama imprime
+Entero = 10
+Flotante = 3.56
+Cadena = "Ed Flores"
+Booleano = True
+print(Entero)
+print(Flotante)
+print(Cadena)
+print(Booleano)
+#La extrano profesor, como la recupero digame como? 
