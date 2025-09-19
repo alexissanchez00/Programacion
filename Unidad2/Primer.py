9d4590c0caf7050905fb890173f6aeff05b6f6f2
alexissanchez00 <cesaralexsanchezcorrea48@gmail.com>
2025-09-19 08:05:23 -0600

    Create Primer.py

---
+++ Unidad2/Primer.py
@@ -0,0 +1,14 @@
+Zoe = []
+Yare = input(" Agrega tu nombre: ")
+Zoe.append(Yare)
+Yare1 = input("Agrega tu lugar de estudio nivel medio superior: ")
+Zoe.append(Yare1)
+Yare2 = input("Agrega tu lugar de residencia: ")
+Zoe.append(Yare2)
+Yare3 = input("Agrega tu edad: ")
+Zoe.append(Yare3)
+Yare4 = input("Agrega la carrera: ")
+Zoe.append(Yare4)
+for Yare in Zoe:
+    print(f"- {Yare}")
+print("\n ¡Lista creada con éxito!")
